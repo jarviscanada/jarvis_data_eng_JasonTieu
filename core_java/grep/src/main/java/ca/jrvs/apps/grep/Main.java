6a37deb63d7f0f1183425352723b9f1cf47cb45b
Jason Tieu <9198332+PseudocodeJason@users.noreply.github.com>
2026-03-18 10:35:54 -0400

    Added log4j support

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/Main.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/Main.java
@@ -1,117 +1,150 @@
-package ca.jrvs.apps.grep;
+    package ca.jrvs.apps.grep;
 
-import java.util.List;
-import java.io.IOException;
-import java.util.ArrayList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+    import java.util.List;
+    import java.io.BufferedReader;
+    import java.io.BufferedWriter;
+    import java.io.File;
+    import java.io.FileReader;
+    import java.io.FileWriter;
+    import java.io.IOException;
+    import java.util.ArrayList;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
 
-public class Main implements JavaGrep {
+    public class Main implements JavaGrep {
 
-    private String rootPath;
-    private String regex;
-    private String outFile;
-    private List<String> matchedLines;
-    private List<String> files;
-    private Logger logger = LoggerFactory.getLogger(Main.class);
+        private String rootPath;
+        private String regex;
+        private String outFile;
+        private List<String> matchedLines;
+        private List<String> files;
+        private Logger logger = LoggerFactory.getLogger(Main.class);
 
-    @Override
-    public void setRootPath(String rootPath) {
-        this.rootPath = rootPath;
-    }
+        @Override
+        public void setRootPath(String rootPath) {
+            this.rootPath = rootPath;
+        }
 
-    @Override
-    public void setRegex(String regex) {
-        this.regex = regex;
-    }
+        @Override
+        public void setRegex(String regex) {
+            this.regex = regex;
+        }
 
-    @Override
-    public void setOutFile(String outFile) {
-        this.outFile = outFile;
-    }
+        @Override
+        public void setOutFile(String outFile) {
+            this.outFile = outFile;
+        }
 
-    @Override
-    public String getRootPath() {
-        return this.rootPath;
-    }
+        @Override
+        public String getRootPath() {
+            return this.rootPath;
+        }
 
-    @Override
-    public String getRegex() {
-        return this.regex;
-    }
+        @Override
+        public String getRegex() {
+            return this.regex;
+        }
 
-    @Override
-    public String getOutFile() {
-        return this.outFile;
-    }
+        @Override
+        public String getOutFile() {
+            return this.outFile;
+        }
 
-    @Override
-    public void process() {
-        logger.info("Starting process method");
-        files = new ArrayList<>();
-        matchedLines = new ArrayList<>();
+        @Override
+        public void process() {
+            logger.info("Starting process method");
 
-        listFiles(rootPath);
-        for (String file : files) {
-            List<String> lines = readLines(file);
-            for (String line : lines) {
-                if (containsPattern(line)) {
-                    matchedLines.add(line);
+            files = new ArrayList<>();
+            matchedLines = new ArrayList<>();
+
+            listFiles(rootPath);
+
+            logger.info("Files found: " + files.size());
+
+            for (String file : files) {
+                logger.info("Processing file: " + file);
+
+                List<String> lines = readLines(file);
+                logger.info("Lines read: " + lines.size());
+
+                for (String line : lines) {
+                    if (containsPattern(line)) {
+                        matchedLines.add(line);
+                    }
                 }
             }
-        }
-        writeToFile(matchedLines);
-        logger.info("Finished process method");
 
-    }
+            logger.info("Matched lines: " + matchedLines.size());
 
-    @Override
-    public List<String> listFiles(String rootDir) {
-        logger.info("Listing files in directory: " + rootDir);
-        for (String file : files) {
-            logger.info("Found file: " + file);
-            files.add(file);
-        }
-        return null;
-    }
-
-    @Override
-    public List<String> readLines(String file) {
-        logger.info("Reading lines from file: " + file);
-        for (String line : matchedLines) {
-            logger.info("Read line: " + line);
-            matchedLines.add(line);
-        }
-        return null;
-    }
-
-    @Override
-    public boolean containsPattern(String line) {
-        logger.info("Checking if line contains pattern: " + line);
-        boolean contains = line.matches(".*" + regex + ".*");
-        logger.info("Line contains pattern: " + contains);
-        return contains;
-    }
-
-    @Override
-    public void writeToFile(List<String> lines) {
-        logger.info("Writing lines to file: " + outFile);
-        for (String line : lines) {
-            logger.info("Writing line: " + line);
-        }
-    }
-
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: java Main regex rootPath outFile");
-            System.exit(1);
+            writeToFile(matchedLines);
         }
 
-        Main grep = new Main();
-        grep.setRegex(args[0]);
-        grep.setRootPath(args[1]);
-        grep.setOutFile(args[2]);
+        @Override
+        public List<String> listFiles(String rootDir) {
+            File root = new File(rootDir);
 
-        grep.process();
+            if (root.isFile()) {
+                files.add(root.getAbsolutePath());
+            } else if (root.isDirectory()) {
+                File[] fileList = root.listFiles();
+                if (fileList != null) {
+                    for (File file : fileList) {
+                        listFiles(file.getAbsolutePath());
+                    }
+                }
+            }
+
+            return files;
+        }
+
+        @Override
+        public List<String> readLines(String file) {
+            List<String> lines = new ArrayList<>();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    lines.add(line);
+                }
+            } catch (IOException e) {
+                logger.error("Error reading file: " + file, e);
+            }
+
+            return lines;
+        }
+
+        @Override
+        public boolean containsPattern(String line) {
+            logger.info("Checking if line contains pattern: " + line);
+            boolean contains = line.matches(".*" + regex + ".*");
+            logger.info("Line contains pattern: " + contains);
+            return contains;
+        }
+
+        @Override
+        public void writeToFile(List<String> lines) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {
+                for (String line : lines) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                logger.error("Error writing to file: " + outFile, e);
+            }
+        }
+
+        public static void main(String[] args) {
+            
+            if (args.length != 3) {
+                System.out.println("Usage: java Main regex rootPath outFile");
+                System.exit(1);
+            }
+
+            Main grep = new Main();
+            grep.setRegex(args[0]);
+            grep.setRootPath(args[1]);
+            grep.setOutFile(args[2]);
+
+            grep.process();
+        }
     }
-}
